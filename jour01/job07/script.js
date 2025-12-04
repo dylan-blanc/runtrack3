6b3a13a920ff9bc5852208f3c06c70ac0aa52ec4
Dylan <dylan.blanc@laplateforme.io>
2025-12-04 14:48:13 +0100

    jours decaler, ajouter conditions selon le mois

--- jour01/job07/script.js
+++ jour01/job07/script.js
@@ -1,22 +1,32 @@
 function jourtravaille() {
-    const annee = 2020;
-    
-    const MoisNumerique = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-    const JoursFerieEnNumerique = [
-        1, 103, 121, 128, 141, 152,
-        195, 227, 305, 315, 359
-    ]
+  const annee = 2020;
+  const JoursFerieEnNumerique = [
+    1, 103, 121, 128, 141, 152, 195, 227, 305, 315, 359,
+  ];
 
+  for (let jour = 1; jour <= 365; jour++) {
+    let mois = 1,
+      jourDuMois = jour;
+    for (; mois <= 12; mois++) {
+      const joursDansMois = new Date(annee, mois, 0).getDate();
+      if (jourDuMois > joursDansMois) {
+        jourDuMois -= joursDansMois;
+      } else {
+        break;
+      }
+    }
 
-for (let jour = 1; jour <= 365; jour++) {
-    const date = new Date(annee, 0);
-    date.setDate(jour);
+    const date = new Date(jourDuMois, mois - 1, annee);
+    const estWeekend = date.getDay() === 0 || date.getDay() === 6;
+    const estJourFerie = JoursFerieEnNumerique.includes(jour);
+
+    if (estWeekend) {
+      console.log(`${jourDuMois}/${mois}/${annee} est un weekend.`);
+    } else if (estJourFerie) {
+      console.log(`${jourDuMois}/${mois}/${annee} est un jour férié.`);
+    } else {
+      console.log(`${jourDuMois}/${mois}/${annee} est un jour travaillé.`);
+    }
+  }
 }
-}
-
 jourtravaille();
-
-
-
-
-
