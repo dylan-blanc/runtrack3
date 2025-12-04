e8dfa3ca446f1494e20b4b37fc8edf37da6b8f13
Dylan <dylan.blanc@laplateforme.io>
2025-12-04 11:28:40 +0100

    job 3-4

--- jour01/job02/index.php
+++ jour01/job02/index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Job02</title>
-    <script src="script.js"></script>
+    <script src="./script.js"></script>
 </head>
 
 <body>
---
+++ jour01/job05/script.js
@@ -0,0 +1,9 @@
+function bisextile(annee) {
+  if ((annee % 4 === 0 && annee % 100 !== 0) || annee % 400 === 0) return true;
+  else {
+    return false;
+  }
+}
+
+console.log(bisextile(2020)); // true
+console.log(bisextile(1900)); // false
