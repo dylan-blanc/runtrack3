c3507c1e9d8e69705f7e6add0d915a4adfd04af8
Dylan <dylan.blanc@laplateforme.io>
2025-12-11 15:24:43 +0100

    modif ajout 2 version

--- jour04/job04/script.js
+++ jour04/job04/script.js
@@ -1,12 +1,36 @@
+document.getElementById("button").addEventListener("click", function () {
+  fetch("users.php")
+    .then((Response) => Response.json())
+    .then((data) => {
+      const tbody = document.getElementById("table");
+      tbody.innerHTML = ""; // vide le contenu du tableau
+      data.forEach((users) => {
+        const tr = document.createElement("tr");
+        tr.innerHTML = `
+                    <td>${users.id}</td>
+                    <td>${users.nom}</td>
+                    <td>${users.prenom}</td>
+                    <td>${users.email}</td>
+                `;
+        tbody.appendChild(tr);
+      });
+    });
+});
+
+
+/*
+* ** version 2 **
+*/
+
 function createRow(user) {
-  const tr = document.createElement("tr");
+  const tr = document.createElement("tr"); // creation d'une ligne du tableau
   tr.innerHTML = `
     <td>${user.id}</td>
     <td>${user.nom}</td>
     <td>${user.prenom}</td>
     <td>${user.email}</td>
   `;
-  return tr;
+  return tr; // fermeture d'une ligne du tableau, avec les données insérées
 }
 
 document.getElementById("button").addEventListener("click", function () {
@@ -14,7 +38,7 @@ document.getElementById("button").addEventListener("click", function () {
     .then((response) => response.json())
     .then((data) => {
       const tbody = document.getElementById("table");
-      tbody.innerHTML = "";
+      tbody.innerHTML = ""; // vide le tableau
       data.forEach((user) => tbody.appendChild(createRow(user)));
     });
 });
